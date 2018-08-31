2cb0d7b568e6ba38f2e07c20e884581aba39cff5
Jian Loong Liew <JianLoong@users.noreply.github.com>
2018-08-31 11:06:36 +1000

    Updated view model

--- Solutions/FIT5032_Week08A/FIT5032_Week08A/Models/SendEmailViewModel.cs
+++ Solutions/FIT5032_Week08A/FIT5032_Week08A/Models/SendEmailViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace FIT5032_Week08A.Models
 {
